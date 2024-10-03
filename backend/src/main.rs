8a9dbc9df3c8380df5b393e6748bfe659d357908
Campbell Reid <campbell@campbellreid.com>
2024-10-03 16:57:37 +1000

    Generated the static build and the backend delivers the content successfully

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,16 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use actix_files::Files;
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
+// #[get("/hello/{name}")]
+// async fn greet(name: web::Path<String>) -> impl Responder {
+//     format!("Hello {name}!")
+// }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(greet)
+        App::new()
+            .service(Files::new("/", "./static").index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
